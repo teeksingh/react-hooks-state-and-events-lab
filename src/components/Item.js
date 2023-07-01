fb9c81b8e6e6f081d7445e3649c6a32e1e41e73d
Prateek <teeksingh@gmail.com>
2023-06-30 22:40:48 -0500

    Add to cart functionality done

--- src/components/Item.js
+++ src/components/Item.js
@@ -1,11 +1,23 @@
 import React from "react";
-
+import { useState } from "react";
+//import items from "./data";
+//console.log(items)
 function Item({ name, category }) {
+
+  const [inCart, changeCart] = useState(false)
+
+  function cartHandler(){
+    changeCart((inCart) => !inCart)
+  }
+
+  const isInCart = inCart ? document.getElementsByTagName("li").className="in-cart" : document.getElementsByTagName("li").className=""
+  const cartStatus = inCart ? "Remove from Cart" : "Add to Cart"
+
   return (
-    <li className="">
+    <li className={isInCart}>
       <span>{name}</span>
       <span className="category">{category}</span>
-      <button className="add">Add to Cart</button>
+      <button onClick={cartHandler} className="add">{cartStatus}</button>
     </li>
   );
 }
