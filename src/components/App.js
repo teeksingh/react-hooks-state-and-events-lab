a7644dbad616db6ec63c156be44c2d8b697fc8c9
Prateek <teeksingh@gmail.com>
2023-06-30 22:12:22 -0500

    Mode button functional

--- src/components/App.js
+++ src/components/App.js
@@ -1,18 +1,26 @@
 import React from "react";
 import ShoppingList from "./ShoppingList";
 import itemData from "../data/items";
+import { useState } from "react";
+
 
 function App() {
 
   // replace 'false' with a state variable that can be toggled between true and false
   // this will be used for the Dark Mode Toggle feature
-  const appClass = false ? "App dark" : "App light"
+  const [isOn, changeMode] = useState(false)
+
+  function toggleMode (){
+    changeMode((isOn) => !isOn)
+  }
+  const appClass = isOn ? "App dark" : "App light"
+  const modeName = isOn ? "Dark Mode" : "Light Mode"
 
   return (
     <div className={appClass}>
       <header>
         <h2>Shopster</h2>
-        <button>Dark Mode</button>
+        <button onClick={toggleMode}>{modeName}</button>
       </header>
       <ShoppingList items={itemData} />
     </div>
